5656a27b449503f5b0868d8564938f851917a724
Jack Korabelnikov <jack.korabelnikov@dscout.com>
2020-03-16 17:17:21 -0500

    day 1 part 2 solved

--- adventofcode/lib/adventofcode.ex
+++ adventofcode/lib/adventofcode.ex
@@ -4,10 +4,32 @@ defmodule Adventofcode do
     """
 
     def day1_part1() do
-        mass = File.read!("lib/input.day1") |> String.split("\n", trim: true)
-        Enum.map(mass, &(div(String.to_integer(&1), 3) - 2)) |> Enum.reduce(&+/2)
+        mass = File.read!("lib/input.day1") |> String.split("\n", trim: true) |> Enum.map(&String.to_integer/1)
+        Enum.map(mass, &calc_fuel/1) |> Enum.reduce(&+/2)
     end
 
+    def day1_part2() do
+        mass = File.read!("lib/input.day1") |> String.split("\n", trim: true) |> Enum.map(&String.to_integer/1)
+        Enum.map(mass, &calc_total_fuel/1) |> Enum.reduce(&+/2)
+    end
+
+    def calc_total_fuel(mass), do: calc_total_fuel(mass, 0)
+
+    defp calc_total_fuel(mass, accum) do
+        fuel = calc_fuel(mass)
+        cond do
+            fuel == 0 -> accum
+            true -> calc_total_fuel(fuel, accum + fuel)
+        end
+    end
+
+    defp calc_fuel(mass) do
+        fuel = div(mass, 3) - 2
+        cond do
+            fuel <= 0 -> 0
+            true -> fuel
+        end
+    end
 end
 
-IO.puts Adventofcode.day1_part1
+IO.puts Adventofcode.day1_part2
--- adventofcode/test/adventofcode_test.exs
+++ adventofcode/test/adventofcode_test.exs
@@ -5,4 +5,13 @@ defmodule AdventofcodeTest do
   test "day 1, part 1" do
     assert Adventofcode.day1_part1() == 3285627
   end
+
+  # test "day 1, part 2" do
+  #   assert Adventofcode.day1_part1() == 3285627
+  # end
+
+  test "calculate total fuel" do
+    assert Adventofcode.calc_total_fuel(14) == 2
+    assert Adventofcode.calc_total_fuel(100756) == 50346
+  end
 end
