e744c6ee01f906d67a6e6f93b899e5fbd5b7bb1f
Jack Korabelnikov <jack.korabelnikov@dscout.com>
2020-03-22 18:02:13 -0500

    created an umbrella app

--- kv_umbrella/apps/keyvalue/mix.exs
+++ kv_umbrella/apps/keyvalue/mix.exs
@@ -4,6 +4,10 @@ defmodule KeyValue.MixProject do
   def project do
     [
       app: :keyvalue,
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
